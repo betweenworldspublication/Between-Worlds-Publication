8644cc46b4f1ec592ca0ed759c5d0ae28b096a0d
Justin Wang <justinw071019@gmail.com>
2026-04-21 15:48:43 -0700

    update mailing 2

--- lib/sanityWrite.ts
+++ lib/sanityWrite.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@sanity/client";
 
 export const sanityWriteClient = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+    projectId: "ftoag85l",
+    dataset: "production",
   apiVersion: "2025-01-01",
   token: process.env.SANITY_API_WRITE_TOKEN,
   useCdn: false,
